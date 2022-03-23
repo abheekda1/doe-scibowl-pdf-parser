c868028b89042a99e9fd34f52c3fcec8ddae5ffb
Abheek Dhawan <abheekd@protonmail.com>
2022-03-22 19:38:57 -0500

    Add basic comments and fix JSON format in program

--- main.go
+++ main.go
@@ -23,11 +23,14 @@ type Question struct {
 }
 
 func main() {
+	// Check arg length to see if at least one argument was supplied, if not then panic
 	argLength := len(os.Args[1:])
 	if argLength < 1 {
-		panic(fmt.Errorf("Failed to detect file"))
+		panic(fmt.Errorf("failed to detect file"))
 	}
 
+	// Set PDF file to the first argument, output file is
+	// either the pdf file
 	pdfFile := os.Args[1]
 	outFile := strings.TrimSuffix(pdfFile, ".pdf") + ".json"
 
@@ -36,10 +39,11 @@ func main() {
 		panic(err)
 	}
 
-  content = strings.ReplaceAll(content, "~", "")
+	content = strings.ReplaceAll(content, "~", "")
 
 	var formattedQuestions []Question
 
+	// Splitting by "TOSS-UP " will allow looping though each question
 	questionList := strings.Split(strings.ReplaceAll(strings.ReplaceAll(content, "\n", ""), "  ", " "), "TOSS-UP ")
 	for i := 1; i < len(questionList); i++ {
 		q, err := getQuestionObj(questionList[i])
@@ -54,17 +58,18 @@ func main() {
 		formattedQuestions = append(formattedQuestions, *q)
 	}
 
-	questionJson, err := json.MarshalIndent(formattedQuestions, " ", " ")
+	// Create a JSON array object from the array of question
+	questionJson, err := json.MarshalIndent(formattedQuestions, "", "  ")
 	if err != nil {
 		panic(err)
 	}
 
+	// Save the JSON to a file to be later used elsewhere
 	ioutil.WriteFile(outFile, questionJson, os.ModePerm)
-
-	return
 }
 
 func getQuestionObj(question string) (*Question, error) {
+	// List of possible categories, could maybe be expanded
 	categories := []string{
 		"BIOLOGY",
 		"CHEMISTRY",
@@ -77,12 +82,16 @@ func getQuestionObj(question string) (*Question, error) {
 		"ASTRONOMY",
 	}
 
-	TU := strings.SplitN(question, "BONUS ", 2)[0]
-	B := strings.SplitN(question, "BONUS ", 2)[1]
+	TU := strings.SplitN(question, "BONUS ", 2)[0] // Toss-Up question
+	B := strings.SplitN(question, "BONUS ", 2)[1]  // Bonus question
 
+	/* Category is an empty string to begin with
+	 * and is defined by looping through valid categories
+	 * until a case-insensitive regex match is found
+	 */
 	category := ""
 	for _, cat := range categories {
-    catExp := regexp.MustCompile(`(?i)` + cat)
+		catExp := regexp.MustCompile(`(?i)` + cat) // Case-insensitive version of the category
 		if catExp.MatchString(TU) {
 			category = catExp.FindString(TU)
 			break
@@ -90,21 +99,26 @@ func getQuestionObj(question string) (*Question, error) {
 	}
 
 	if category == "" {
-		return nil, fmt.Errorf("Category not found")
+		return nil, fmt.Errorf("category not found")
 	}
 
+	// Return if it's math as math is currently broken
 	if category == "MATH" {
 		return nil, fmt.Errorf("math")
 	}
 
-  if strings.Contains(TU, category + " –") {
-    TU = strings.Replace(TU, " –", "", 1)
-  }
+	// Replace the first dash from Toss-Ups and bonuses
+	// if it appears after the category, helps with
+	// format detection later on
+	if strings.Contains(TU, category+" –") {
+		TU = strings.Replace(TU, " –", "", 1)
+	}
 
-  if strings.Contains(B, category + " –") {
-    B = strings.Replace(B, " –", "", 1)
-  }
-    
+	if strings.Contains(B, category+" –") {
+		B = strings.Replace(B, " –", "", 1)
+	}
+
+	// Parse the Toss-Up and bonus strings for their data
 	tuFormat := strings.Join(strings.SplitN(strings.SplitN(TU, category+" ", 2)[1], " ", 4)[0:2], " ")
 	tuQuestion := strings.SplitN(strings.SplitN(TU, " ANSWER:", 2)[0], tuFormat+" ", 2)[1]
 	tuAnswer := strings.SplitN(TU, "ANSWER: ", 2)[1]
@@ -113,8 +127,11 @@ func getQuestionObj(question string) (*Question, error) {
 	bQuestion := strings.SplitN(strings.SplitN(B, " ANSWER:", 2)[0], bFormat+" ", 2)[1]
 	bAnswer := strings.SplitN(B, "ANSWER: ", 2)[1]
 
+	// Remove the footer which often trails the bonus answer as it's on the bottom of the page
+	// FIXME: some formats still don't get picked up by this regex
 	footerExp := regexp.MustCompile(`(?i)\s*(High School |Middle School |\d+\s*Regional.*)?Round \d.*`)
 
+	// Create a question object to return
 	q := Question{
 		Category:       strings.ToUpper(strings.TrimSpace(category)),
 		TossupFormat:   strings.TrimSpace(tuFormat),
@@ -128,12 +145,14 @@ func getQuestionObj(question string) (*Question, error) {
 	return &q, nil
 }
 
+// This example is taken from the source of the PDF package in use
 func readPdf(path string) (string, error) {
 	f, r, err := pdf.Open(path)
-	defer f.Close()
 	if err != nil {
 		return "", err
 	}
+
+	defer f.Close()
 	var buf bytes.Buffer
 	b, err := r.GetPlainText()
 	if err != nil {
