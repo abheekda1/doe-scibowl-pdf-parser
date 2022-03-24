54a7d884aa50060062026ab259c607d6849f9b25
Abheek Dhawan <abheekd@protonmail.com>
2022-03-24 17:29:30 -0500

    Add test for question string parsing

---
+++ parse/parse_test.go
@@ -0,0 +1,24 @@
+package parse
+
+import "testing"
+
+func TestParse(t *testing.T) {
+	got, err := GetQuestionObj("1) Chemistry – Multiple Choice In a polyelectronic atom, which of the following orbitals would have the lowest energy? W) 3s X) 4s Y) 3p Z) 3d ANSWER: W) 3s BONUS 1) Chemistry – Short Answer Rank the following three carboxylic [CAR-box-IHL-ik] acid derivatives in terms of increasing reactivity to nucleophilic substitution: 1) Amide; 2) Acid anhydride; 3) Ester. ANSWER: 1, 3, 2")
+	want := Question{
+		Category:       "CHEMISTRY",
+		TossupFormat:   "Multiple Choice",
+		TossupQuestion: "In a polyelectronic atom, which of the following orbitals would have the lowest energy? W) 3s X) 4s Y) 3p Z) 3d",
+		TossupAnswer:   "W) 3s",
+		BonusFormat:    "Short Answer",
+		BonusQuestion:  "Rank the following three carboxylic [CAR-box-IHL-ik] acid derivatives in terms of increasing reactivity to nucleophilic substitution: 1) Amide; 2) Acid anhydride; 3) Ester.",
+		BonusAnswer:    "1, 3, 2",
+	}
+
+	if err != nil {
+		t.Errorf("encountered an error: %q", err)
+	}
+
+	if *got != want {
+		t.Errorf("got: %q, wanted: %q", got, want)
+	}
+}
